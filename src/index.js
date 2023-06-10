d9bf8328e89f7dab4b91ac6a860dce5d616ce00a
Andrea Cabrera <andreacabrera@MacBook-Air-de-Andrea-2.local>
2023-06-10 11:19:16 -0600

    reset button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const el = document.getElementById("reset-button");
+el.addEventListener("click", fun)
+
+function fun () {
+   document.getElementById("myForm").value="";
+}
+
+const item = document.getElementById("myForm");
+item.addEventListener("keyup", update)
+
+function update () {
+   
+}
